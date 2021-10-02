4430de7140c4a8430f2061d221471efefd4a8b8f
codes-untold <talentedsoul74@gmail.com>
2021-10-02 03:19:23 -0700

    update

--- lib/Screens/LoginScreen.dart
+++ lib/Screens/LoginScreen.dart
@@ -162,7 +162,7 @@ class _LoginScreenState extends State<LoginScreen> {
                     ),
                     GestureDetector(
                       onTap: (){
-                        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context){
+                        Navigator.push(context, MaterialPageRoute(builder: (context){
                           return RegisterScreen();
                         }));
                       },
